9a751ddd7c56e249439f37adf5f474518de90b56
hideaking2021 <hide.freetimes.2021@gmail.com>
2022-02-21 01:58:59 +0900

    アロー関数

--- ja/main.js
+++ ja/main.js
@@ -5,13 +5,22 @@
 //   return a + b + c;
 // }
 
-const sum = function(a, b, c) {
-  // console.log(a + b + c);
-  return a + b + c;
-};
+// const sum = function(a, b, c) {
+//   // console.log(a + b + c);
+//   return a + b + c;
+// };
+
+// const sum = (a, b, c) =>  a + b + c;
 
 
-const total = sum(1, 2, 3) + sum(3, 4, 5);
-console.log(total);
 
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+// console.log(total);
+
+// const double = function(a) {
+//   return a * 2;
+// }
+
+const double = a => a * 2;
+console.log(double(12));
 
