e7d629b5bafc9122ae8dcde9a0f6d1e06f1d9102
hideaking2021 <hide.freetimes.2021@gmail.com>
2022-02-22 00:59:28 +0900

    .

--- ja/main.js
+++ ja/main.js
@@ -1,6 +1,24 @@
 'use strict';
 
 {
-  const x = 100;
-console.log(x);
+  const point = {
+    x: 100,
+    y: 180,
+  };
+
+  // const keys = Object.keys(point);
+  // keys.forEach(key => {
+  //   console.log(`Key: ${key} Value: ${point[key]}`);
+  // });
+
+  const points = [
+    {x: 30, y: 20},
+    {x: 10, y: 50},
+    {x: 40, y: 40},
+];
+
+console.log(points[1].y);
+
 }
+
+
